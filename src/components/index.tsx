3b4ac1c0eb5247e9b04a318c50ba0a71bf3ce67f
Ratchanon0533 <ratchanon.tg2548@gmail.com>
2025-11-25 14:03:38 +0700

    Add carousel section

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,15 @@
 // main.tsx
 import Nav from './nav-bar';
 import SearchSection from './search-section';
+import Carousel from './carousel-section';
 
 const Main = () => {
     return (
         <>
             <Nav />
             <SearchSection />
+            <Carousel />
+
         </>
     )
 }
