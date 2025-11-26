a8b219bb6bdded07425254afca482b5bf97dbb04
Nah-Null <pgthman@gmail.com>
2025-11-26 20:53:10 +0700

    แก้หน้าHOMEที่จะแสดงหลังจากloginแล้ว

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,24 @@
 // main.tsx
 import Nav from './nav-bar(login)';
 import SearchSection from './search-section';
+import Carousel from './carousel-section';
+import University from './university-section';
+import QuickSearchHome from './quick-search(Home)';
+import PopularCareer from './career';
+import Applying from './applying';
 
 const Home = () => {
     return (
         <>
             <Nav />
             <SearchSection />
+            <Carousel />
+            <University />
+            <QuickSearchHome />
+            <PopularCareer />
+            <Applying />
             หน้าแรกหลังจากloginสำเร็จ
+
         </>
     )
 }
