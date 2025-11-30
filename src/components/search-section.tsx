059d3270b65caf779b506105efcb2b6db0098374
Nah-Null <pgthman@gmail.com>
2025-11-30 19:31:33 +0700

    แก้api

--- src/components/search-section.tsx
+++ src/components/search-section.tsx
@@ -62,7 +62,7 @@ const SearchSection = () => {
         if (inputMajor.trim()) requestBody.major = inputMajor;
 
         try {
-            const response = await fetch('http://localhost:5000/api/search-university', {
+            const response = await fetch('https://daily-life-backend.vercel.app/api/search-university', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(requestBody),
